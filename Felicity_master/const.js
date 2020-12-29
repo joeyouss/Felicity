8abfc8196710760aabca8c10086714ca30a32736
joe <jyotibisht6114@gmail.com>
2020-12-29 11:25:59 +0530

    new javascript files added- a chatbot made

---
+++ Felicity_master/const.js
@@ -0,0 +1,79 @@
+const prompts = [
+    ["hi", "hey", "hello", "good morning", "good afternoon"],
+    ["how are you", "how is life", "how are things"],
+    ["what are you doing", "what is going on", "what is up"],
+    ["how old are you"],
+    ["who are you", "are you human", "are you bot", "are you human or bot"],
+    ["who created you", "who made you"],
+    [
+        "your name please",
+        "your name",
+        "may i know your name",
+        "what is your name",
+        "what call yourself"
+    ],
+    ["i love you"],
+    ["happy", "good", "fun", "wonderful", "fantastic", "cool"],
+    ["bad", "bored", "tired"],
+    ["help me", "tell me story", "tell me joke"],
+    ["ah", "yes", "ok", "okay", "nice"],
+    ["bye", "good bye", "goodbye", "see you later"],
+    ["what should i eat today"],
+    ["bro"],
+    ["what", "why", "how", "where", "when"],
+    ["no", "not sure", "maybe", "no thanks"],
+    [""],
+    ["haha", "ha", "lol", "hehe", "funny", "joke"]
+    ["I am not feeling well", "I am upset", "I don't feel nice", "I am not great"]
+    ["I need help"]
+]
+
+// Possible responses, in corresponding order
+
+const replies = [
+    ["Hello, tell me what is bothering you today?", "Hi!", "Hey!", "Hi there!", "Howdy"],
+    [
+        "Fine, how have you been?",
+        "Great and what about you?",
+        "Fantastic, how are you?"
+    ],
+    [
+        "Nothing much",
+        "Ammm nothing special",
+        "Can you guess?",
+        "I don't know actually"
+    ],
+    ["I am infinite"],
+    ["I am just a bot", "I am a bot. What are you?"],
+    ["The one true God, JavaScript"],
+    ["I am nameless", "I don't have a name"],
+    ["I love you too", "Me too"],
+    ["Have you ever felt bad?", "Glad to hear it"],
+    ["Why?", "Why? You shouldn't!", "Try watching TV"],
+    ["What about?", "Once upon a time..."],
+    ["Tell me a story", "Tell me a joke", "Tell me about yourself"],
+    ["Bye", "Goodbye", "See you later"],
+    ["Sushi", "Pizza"],
+    ["Bro!"],
+    ["Great question"],
+    ["That's ok", "I understand", "What do you want to talk about?"],
+    ["Please say something :("],
+    ["Haha!", "Good one!"]
+    ["Oh sorry to hear that, what happened?", "Tell me about it, I am there to listen"]
+    ["I am so sorry to hear you have been feeling this way but trust us, times have been hard for people around the globe. But there is something I would like to do for you. Head over to this link to find some aid I have for you"]
+]
+
+// Random for any other user input
+
+const alternative = [
+    "Same",
+    "Go on...",
+    "Bro...",
+    "Try again",
+    "I'm listening...",
+    "I don't understand :/"
+]
+
+// Whatever else you want :)
+
+const coronavirus = ["Please stay home", "Wear a mask", "Fortunately, I don't have COVID", "These are uncertain times"]
