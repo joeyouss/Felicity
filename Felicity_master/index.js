37a2c5daf36cd712e872bcb0d0f188827dd562f4
joe <jyotibisht6114@gmail.com>
2021-01-01 09:46:24 +0530

    added javascript files and changed Ui

--- Felicity_master/index.js
+++ Felicity_master/index.js
@@ -77,7 +77,7 @@ function addChat(input, product) {
     let botImg = document.createElement("img");
     let botText = document.createElement("span");
     botDiv.id = "bot";
-    botImg.src = "bot.png";
+    botImg.src = "aiai.jpg";
     botImg.className = "avatar";
     botDiv.className = "bot response";
     botText.innerText = "Typing...";
